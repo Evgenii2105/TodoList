4de9a2f6214db1d47b34fc57f865916c0ea3a2ac
Evgenii2105 <Fomev1@yandex.ru>
2025-05-20 12:26:25 +0300

    Created Repositories

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController.swift
 //  TodoList
 //
 //  Created by Евгений Фомичев on 20.05.2025.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
